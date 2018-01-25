7f1ba37df7d858bf102bfafdccacf2eeb6579377
resfieldpro <resfieldpro@gmail.com>
2018-01-25 21:30:32 +0300

    web production data import

--- FieldPROConnector/App/Brokers/WellAllocationBroker.cs
+++ FieldPROConnector/App/Brokers/WellAllocationBroker.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Slb.Ocean.Petrel;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Odbc;
 using System.Linq;
 using System.Text;
+using WebDataLoader;
 
-namespace FieldPROConnector {
-    class WellAllocationBroker {
+namespace FieldPROConnector
+{
+    class WellAllocationBroker
+    {
 
         private WebConfiguration dcInfo;
         private Frequency frequency;
@@ -16,57 +21,39 @@ namespace FieldPROConnector {
             this.frequency = frequency;
         }
 
-        public List<WellAllocation> GetAll(string uwi, DateTime start, DateTime end) {
+        public List<WellAllocation> GetAll(string uwi, DateTime start, DateTime end)
+        {
             List<WellAllocation> allocations = new List<WellAllocation>();
-            OdbcParameter wellParam = new OdbcParameter("@UWI", uwi);    
-            OdbcParameter startParam = new OdbcParameter("@Start", start);
-            OdbcParameter endParam = new OdbcParameter("@End", end);
-            string sql = String.Format(@"SELECT WELLS.WELL_UWI, WELL_NAME, PROD_DATE, UPTIME_OPERATING_PRODUCING,
-                            OIL_VOLUME, GAS_VOLUME, WATER_VOLUME, PRIMARY_OIL_VOLUME, SECONDARY_OIL_VOLUME,
-                             0 WATER_INJECTED_VOLUME, 0 GAS_INJECTED_VOLUME, 0 WATER_DISPOSAL_VOLUME
-                            FROM {0}, WELLS, DATA_COMPONENTS
-                            WHERE WELLS.WELL_KEY_ID = DATA_COMPONENTS.WELL_KEY_ID
-                             AND DATA_COMPONENTS.DATA_COMPONENT_ID = {0}.DATA_COMPONENT_ID
-                             AND WELLS.WELL_UWI = ?
-                             AND PROD_DATE BETWEEN ? AND ?
-                            ORDER BY PROD_DATE ASC;", this.ProductionTable());
-            var odbcParams = new List<OdbcParameter> {wellParam, startParam, endParam};
-				//using (FiPConnection cn = dcInfo.NewConnection()) {
-				//	 cn.ExecuteSql(sql, odbcParams, (reader) => {
-				//		  var a = this.WellAllocationFromRow(reader);
-				//		  allocations.Add(a);
-				//		  return a;
-				//	 });
-				//}
+            WebImporter wi = new WebImporter();
+            try
+            {
+                DataTable dt = wi.LoadDailyProduction(dcInfo.WebServer, dcInfo.WebServerU, dcInfo.WebServerP, uwi);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    var w = this.WellAllocationFromRow(dr);
+                    allocations.Add(w);
+                }
+            }
+            catch (Exception e)
+            {
+                PetrelLogger.InfoOutputWindow("Failed to load wells: " + e.ToString());
+            }
             return allocations;
         }
 
-        private WellAllocation WellAllocationFromRow(OdbcDataReader reader) {
+        private WellAllocation WellAllocationFromRow(DataRow r)
+        {
             WellAllocation alloc = new WellAllocation();
-            alloc.OperativeDate = reader.GetDateTime(reader.GetOrdinal("PROD_DATE"));
-            var dbOil = reader["OIL_VOLUME"]; // In bbls
-            var dbGas = reader["GAS_VOLUME"]; // In Mscf
-            var dbWater = reader["WATER_VOLUME"]; // In bbls
-            var dbWaterInjection = reader["WATER_INJECTED_VOLUME"]; // In bbls
-            var dbGasInjection = reader["GAS_INJECTED_VOLUME"]; // In Mscf
-            var dbDisposalInjection = reader["WATER_DISPOSAL_VOLUME"]; // In bbls
-            var dbEffectiveTime = reader["UPTIME_OPERATING_PRODUCING"];
+            alloc.OperativeDate = Convert.ToDateTime(r["DATE"]);
+            var dbOil = r["OIL"]; // In bbls
+            var dbGas = r["GAS"]; // In Mscf
+            var dbWater = r["WATER"]; // In bbls
 
             alloc.Oil_Bbls = DBNull.Value.Equals(dbOil) ? 0.0 : double.Parse(dbOil.ToString());
             alloc.Gas_Mscf = DBNull.Value.Equals(dbGas) ? 0.0 : double.Parse(dbGas.ToString());
             alloc.Water_Bbls = DBNull.Value.Equals(dbWater) ? 0.0 : double.Parse(dbWater.ToString());
-            alloc.WaterInjection_Bbls = DBNull.Value.Equals(dbWaterInjection) ? 0.0 : double.Parse(dbWaterInjection.ToString());
-            alloc.DisposalInjection_Bbls = DBNull.Value.Equals(dbDisposalInjection) ? 0.0 : double.Parse(dbDisposalInjection.ToString());
-            alloc.EffectiveTime = DBNull.Value.Equals(dbEffectiveTime) ? 0.0 : double.Parse(dbEffectiveTime.ToString());
 
             return alloc;
-        }
-
-        private string ProductionTable() {
-            if (this.frequency == Frequency.Daily)
-                return "V_PRODUCTION_DAILY_DATA";
-            else
-                return "V_PRODUCTION_MONTHLY_DATA";
         }
     }
 }
